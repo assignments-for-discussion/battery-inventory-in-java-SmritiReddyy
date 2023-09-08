7d8a674a7d3dc1c8fd0f4c2b2afa5f5c39299ac6
SmritiReddyy <91471371+SmritiReddyy@users.noreply.github.com>
2023-09-08 19:12:11 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -9,6 +9,21 @@ public class Main {
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
+    
+    for (int capacity : presentCapacities) {
+      // Convert present capacity to SoH
+      double soh = (double) capacity / 120.0 * 100.0;
+
+      // Classify batteries based on SoH
+      if (soh > 80.0 && soh <= 100.0) {
+        counts.healthy++;
+      } else if (soh >= 65.0 && soh <= 80.0) {
+        counts.exchange++;
+      } else {
+        counts.failed++;
+      }
+    }
+
     return counts;
   }
 
@@ -16,9 +31,17 @@ public class Main {
     System.out.println("Counting batteries by SoH...\n");
     int[] presentCapacities = {115, 118, 80, 95, 91, 77};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+
+    // Print the counts
+    System.out.println("Healthy Batteries: " + counts.healthy);
+    System.out.println("Exchange Batteries: " + counts.exchange);
+    System.out.println("Failed Batteries: " + counts.failed);
+
+    // Existing asserts
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
+    
     System.out.println("Done counting :)\n");
   }
 
